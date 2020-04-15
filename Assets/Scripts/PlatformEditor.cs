04322e6a68ab823e6bf2935b410b07184a837508
JuanDavidPF <JuanFavidPF@gmail.com>
2020-04-14 22:23:45 -0500

    finished the platform prefab, now it has a placeholder for editmode view and generates height aswell, started to work in player's character control and animation

--- Assets/Scripts/PlatformEditor.cs
+++ Assets/Scripts/PlatformEditor.cs
@@ -7,35 +7,45 @@ public class PlatformEditor : MonoBehaviour {
     //Components
     private BoxCollider2D hitbox;
 
-    //Hitbox
-    private Vector2 hitboxDimensions;
-    private Vector2 hitboxPosition;
+    //placeHolder
+    private Vector3 placeHolderDimensions;
+    private Vector3 placeHolderPosition;
+    private int platformLength;
+    private int platformHeight;
 
     //Graphics
-
-    public GameObject material;
-
-    public int platformLength;
+    public Sprite material;
 
     // Start is called before the first frame update
     void Start () {
 
-        hitboxDimensions = new Vector2 (platformLength, 1);
-        hitboxPosition = new Vector2 (platformLength / 2f, -0.5f);
+        //extract the dimensions and position of the placeholder modified in editor view.
+        placeHolderDimensions = transform.GetChild (0).GetComponent<Transform> ().localScale;
+        placeHolderPosition = transform.GetChild (0).GetComponent<Transform> ().position;
+
+        platformLength = (int) Mathf.Round (placeHolderDimensions.x);
+        platformHeight = (int) Mathf.Round (placeHolderDimensions.y);
+
+        //adjust the hitbox to the dimensions of the placeholder
 
         hitbox = GetComponent<BoxCollider2D> ();
-        hitbox.size = hitboxDimensions;
-        hitbox.offset = hitboxPosition;
+        hitbox.size = new Vector2 (platformLength, platformHeight);
+        hitbox.offset = new Vector2 (platformLength / 2f, -(platformHeight / 2f));
 
-        //create tiles   
+        //create the tiles and fill the hitbox with them
 
         for (int i = 0; i < platformLength; i++) {
-            GameObject tile = Instantiate (material);
-            tile.transform.parent = transform;
-            Vector3 tileSortPosition = new Vector3 (i + transform.position.x, transform.position.y, transform.position.z);
-            tile.transform.position = tileSortPosition;
+            for (int j = 0; j < platformHeight; j++) {
+                GameObject tile = new GameObject ("Platform Sprite Tile");
+                tile.transform.parent = transform;
+                tile.transform.position = new Vector3 (i + transform.position.x, -j + transform.position.y, transform.position.z);
+                tile.AddComponent<SpriteRenderer> ();
+                tile.GetComponent<SpriteRenderer> ().sprite = material;
+            }
         }
 
+        //deactivates the placeholder
+        transform.GetChild (0).gameObject.SetActive (false);
     } //close the start method
 
     // Update is called once per frame
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //components
+    private Rigidbody2D physics;
+
+    //movement
+    [Range (1, 100)]
+    public float moveSpeed = 10f;
+    [Range (1, 20)]
+    public float jumpForce = 20f;
+    private float moveDirection;
+
+    public bool isFacingRight = true;
+
+    // Start is called before the first frame update
+    void Start () {
+
+        physics = GetComponent<Rigidbody2D> ();
+
+    } //closes start method
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        moveDirection = Input.GetAxis ("Horizontal");
+        physics.velocity = new Vector2 (moveDirection * moveSpeed, physics.velocity.y);
+
+    } //closes fixedUpdate method
+
+    void Update () {
+
+        if (Input.GetKeyDown ("w")) {
+            physics.AddForce (new Vector2 (0, jumpForce * 150));
+        }
+        if ((moveDirection > 0 && !isFacingRight) || (moveDirection < 0 && isFacingRight)) {
+            FlipSprite ();
+        }
+    }
+
+    void FlipSprite () {
+        isFacingRight = !isFacingRight;
+        float VerticalFlip = transform.localScale.x * -1;
+        transform.localScale = new Vector3 (VerticalFlip, transform.localScale.y, transform.localScale.z);
+
+    }
+} //closes player movement class
