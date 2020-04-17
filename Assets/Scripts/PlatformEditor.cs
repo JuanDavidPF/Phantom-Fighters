cd57f920794827de285329396adf9a4065a2b8dc
JuanDavidPF <JuanFavidPF@gmail.com>
2020-04-17 08:31:34 -0500

    started to work in scripted mobile platforms

--- Assets/Scripts/PlatformEditor.cs
+++ Assets/Scripts/PlatformEditor.cs
@@ -5,19 +5,47 @@ using UnityEngine;
 [SelectionBase]
 public class PlatformEditor : MonoBehaviour {
 
+    //////////////////////Dimensions of the rectangular platform in units///////////////
+
     private int platformLength;
     private int platformHeight;
 
+    /////////////////////////Variables with predifined values///////////////
+    public enum Directions { vertical, horizontal, diagonal1, diagonal2 }
+
+    /////////////////////////Apperance///////////////
+    [Space (5)]
     [Header ("Appearance")]
+    [Tooltip ("Here goes the sprite of the material that hte platform's tiles are going to have")]
     public Sprite material;
 
-    // Start is called before the first frame update
+    /////////////////////////Movement///////////////
+
+    [Space (5)]
+    [Header ("Movement")]
+    [Tooltip ("Allows the platfom to move in a particular direction")]
+    public bool itMoves;
+
+    [Tooltip ("Sets the direction in wich the platform it's going to move")]
+    public Directions direction;
+
+    [Tooltip ("Control of the animation of the platform")]
+    public RuntimeAnimatorController movementController;
+
+    private int indexDirection;
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
     void Start () {
 
+        //determines the size of the platform, the size is in unity units, so the dimensions need to be positive, rounded integers.
         platformLength = (int) Mathf.Round (Mathf.Abs (transform.localScale.x));
         platformHeight = (int) Mathf.Round (Mathf.Abs (transform.localScale.y));
 
-        //fixes the position of the platform if is negative scaled
+        //fixes the platform corner position if the object was negative scaled
         if (transform.localScale.x < 0) {
             transform.position = new Vector3 (transform.localScale.x + transform.position.x, transform.position.y, transform.position.z);
             transform.localScale = new Vector3 (Mathf.Abs (transform.localScale.x), transform.localScale.y, transform.localScale.z);
@@ -28,7 +56,7 @@ public class PlatformEditor : MonoBehaviour {
             transform.localScale = new Vector3 (transform.localScale.x, Mathf.Abs (transform.localScale.y), transform.localScale.z);
         }
 
-        //create the tiles and fill the platform with them
+        //create each tile and fills the whole collider with them
 
         for (int i = 0; i < platformLength; i++) {
             for (int j = 0; j < platformHeight; j++) {
@@ -40,12 +68,43 @@ public class PlatformEditor : MonoBehaviour {
             }
         }
 
-        //deactivates the placeholder if there is a material set to it
+        //deactivates the placeholder if a sprite material was atached to the platform
 
         if (material != null) {
             transform.GetChild (0).gameObject.SetActive (false);
         }
 
-    } //close the start method
+        //creates the animator component and moves the platform to aparent gameobject to unfix the platform animated original position
+        if (itMoves) {
 
-}
+            GameObject mobilePlatform = new GameObject ("MobilePlatform");
+            mobilePlatform.transform.position = transform.position;
+            transform.parent = mobilePlatform.transform;
+            gameObject.AddComponent<Animator> ();
+            Animator animate = GetComponent<Animator> ();
+            animate.runtimeAnimatorController = movementController;
+
+            switch (direction) {
+
+                case Directions.vertical:
+                    indexDirection = 1;
+                    break;
+                case Directions.horizontal:
+                    indexDirection = 2;
+                    break;
+                case Directions.diagonal1:
+                    indexDirection = 3;
+                    break;
+                case Directions.diagonal2:
+                    indexDirection = 4;
+                    break;
+            }
+            animate.SetInteger ("direction", indexDirection);
+        }
+    } //close the start method
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+} //closes the class PlatformEditor
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,50 +5,92 @@ using UnityEngine;
 [SelectionBase]
 public class PlayerMovement : MonoBehaviour {
 
-    //components
-    private Rigidbody2D physics;
-    private Animator animate;
-
-    public enum Players { player1, player2 }
-    public enum Characters { Kiryl, Scafander, Death }
-
-    [Header ("Character Identifier")]
-
-    public Players playerIndex;
-    public Characters character;
-
-    [Header ("Movement")]
-    [Range (1, 100)]
-    public float moveSpeed;
-    [Range (1, 20)]
-    public float jumpForce;
-    private float jumpCharge;
-    public float jumpChargeMax;
-    public bool isJumpCoroutineRunning;
-
-    private float moveDirection;
-    public bool isFacingRight = true;
-
-    [Header ("Grounded")]
-    public Transform groundCheck;
-    private bool isGrounded;
-
-    [Range (0.1f, 0.5f)]
-    public float groundCheckRadius = 0.2f;
-    public LayerMask whatIsGround;
+    /////////////////////////Global determinations///////////////
 
     [HideInInspector]
     public bool isGamePaused;
 
+    /////////////////////////Components///////////////
+    private Rigidbody2D physics;
+    private Animator animate;
+
+    /////////////////////////Variables with predifined values///////////////
+
+    public enum Players { player1, player2 }
+
+    public enum Characters { Kiryl, Scafander, Death }
+
+    /////////////////////////Player control////////////////////////////////
+
+    [Space (5)]
+    [Header ("Character Identifier")]
+    [Tooltip ("Determines what player is controlling the character")]
+    public Players playerIndex;
+
+    [Tooltip ("Determines what mechanics your character can perform")]
+    public Characters character;
+
+    /////////////////////////Horizontal Movement////////////////////////////////
+    [Space (5)]
+    [Header ("Movement")]
+
+    [Tooltip ("Determines the speed of horizontal movement")]
+    [Range (1, 100)]
+    public float moveSpeed;
+
+    [Tooltip ("True if the player is moving right. Check if the sprite is looking to the right, Uncheck if the sprite is looking to the left.")]
+    public bool isFacingRight = true;
+
+    private float moveDirection;
+
+    /////////////////////////Vertical Movement////////////////////////////////
+    [Space (5)]
+    [Header ("Jump")]
+    [Tooltip ("Determines how much base force the jump has")]
+    [Range (1, 20)]
+    public float jumpForce;
+
+    [Tooltip ("Determines how many seconds the jump button needs to be pressed to reach max height jump")]
+    [Range (0, 5)]
+    public float jumpChargeTimer;
+    private float jumpCharge;
+    private IEnumerator jumpCoroutine;
+
+    private bool isJumpCoroutineRunning;
+
+    [Tooltip ("Determines the range of the jump boost, the X determines the lowest jump boost possible and the Y the Maximun jump boost posibble")]
+
+    public Vector2 jumpBoostInterval;
+
+    /////////////////////////Ground Detection////////////////////////////////
+    [Space (5)]
+    [Header ("Ground Detection")]
+    [Tooltip ("Trigger that detects contact with the ground")]
+    public Transform groundCheck;
+
+    [Tooltip ("Trigger that determines the size of the ground trigger")]
+    [Range (0.1f, 0.5f)]
+    public float groundCheckRadius = 0.2f;
+
+    [Tooltip ("Determines what layers the trigger will count as a ground")]
+    public LayerMask whatIsGround;
+    private bool isGrounded;
+
+    /////////////////////////////Controls////////////////////////////////
     private string jumpBtn;
     private string WalkBtn;
-    // Start is called before the first frame update
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+
     void Start () {
 
+        //link components
         physics = GetComponent<Rigidbody2D> ();
         animate = GetComponentInChildren<Animator> ();
 
-        //mapeo de los controles
+        //Control mapping determined by which player is in control 
         switch (playerIndex) {
             case Players.player1:
 
@@ -60,83 +102,132 @@ public class PlayerMovement : MonoBehaviour {
                 jumpBtn = "Player2Jump";
                 WalkBtn = "Player2Horizontal";
                 break;
-
         }
 
+        //converts seconds to frames
+        jumpChargeTimer = jumpChargeTimer * 60;
     } //closes start method
 
-    // Update is called once per frame
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+
     void FixedUpdate () {
 
-        //move the player horizontally
+        //////////////////////////////////Move Conditions///////////////////////////////////
 
-        moveDirection = Input.GetAxis (WalkBtn);
-        animate.SetFloat ("run", Mathf.Abs (moveDirection));
-        physics.velocity = new Vector2 (moveDirection * moveSpeed, physics.velocity.y);
+        Move ();
 
-        //check if the player is on the ground
-        isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
-        animate.SetBool ("grounded", isGrounded);
+        //////////////////////////////////Ground Check///////////////////////////////////
 
-        //check if the player is in midair rising or falling
-        animate.SetFloat ("verticalDirection", physics.velocity.y);
+        Grounded ();
 
     } //closes fixedUpdate method
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
 
     void Update () {
 
         if (!isGamePaused) {
 
+            //flip the sprite in the direction the player is facing
+            if ((moveDirection > 0 && !isFacingRight) || (moveDirection < 0 && isFacingRight)) {
+                FlipSprite ();
+            }
+
+            //animate if the player on the air is Rising or Falling
+            if (!isGrounded) animate.SetFloat ("verticalDirection", physics.velocity.y);
+
             if (isGrounded) {
 
+                //////////////////////////////////Jump Conditions///////////////////////////////////
+
+                //resets the jumping state everytime the jumpbtn is pressed
                 if (Input.GetButtonDown (jumpBtn)) {
+
+                    if (jumpCoroutine != null) {
+                        StopCoroutine (jumpCoroutine);
+                        jumpCoroutine = null;
+                    }
                     jumpCharge = 0;
-                    isJumpCoroutineRunning = false;
+
                 }
 
-                //hold and jump
-                if (Input.GetButton (jumpBtn) && isJumpCoroutineRunning == false) {
-                    jumpCharge += 0.5f;
-                    if (jumpCharge >= jumpChargeMax) {
-                        isJumpCoroutineRunning = true;
-                        StopCoroutine ("Jump");
-                        StartCoroutine (Jump ());
+                //Jump height increases if  the player holds the jump button
+                if (Input.GetButton (jumpBtn) && jumpCoroutine == null) {
+                    jumpCharge++;
+
+                    if (jumpCharge >= jumpChargeTimer) {
+                        jumpCoroutine = Jump ();
+                        StartCoroutine (jumpCoroutine);
+
                     }
                 }
+                //Jump is less strong if the player taps and releases the jump button
+                else if (Input.GetButtonUp (jumpBtn) && jumpCharge < jumpChargeTimer && jumpCoroutine == null) {
 
-                //Tap and Jump
-                if (Input.GetButtonUp (jumpBtn) && jumpCharge < jumpChargeMax && isJumpCoroutineRunning == false) {
-                    isJumpCoroutineRunning = true;
-                    StopCoroutine ("jump");
-                    StartCoroutine (Jump ());
+                    //dimishes in a 35% the jump boost charged before the button was released
+                    jumpCharge = jumpCharge - jumpCharge * 0.35f;
+                    jumpCoroutine = Jump ();
+                    StartCoroutine (jumpCoroutine);
 
                 }
-            }
-        }
+            } //closes the grounded condition
 
-        //flip the sprite in the direction is facing
-        if ((moveDirection > 0 && !isFacingRight) || (moveDirection < 0 && isFacingRight)) {
-            FlipSprite ();
-        }
-    }
+        } //closes the pause condition
+
+    } //closes the update method
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
 
     void FlipSprite () {
         isFacingRight = !isFacingRight;
         float VerticalFlip = transform.localScale.x * -1;
         transform.localScale = new Vector3 (VerticalFlip, transform.localScale.y, transform.localScale.z);
-
     }
 
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
     IEnumerator Jump () {
-
-        jumpCharge = Map (jumpCharge, 0, jumpChargeMax, 120, 170);
+        jumpCharge = ChangeNumberScale (jumpCharge, 0, jumpChargeTimer, jumpBoostInterval.x, jumpBoostInterval.y);
         physics.AddForce (new Vector2 (0, jumpForce * jumpCharge));
         jumpCharge = 0;
         yield return null;
     }
 
-    float Map (float value, float fromMin, float fromMax, float toMin, float toMax) {
-        return toMin + (value - fromMin) * (toMax - toMin) / (fromMax - fromMin);
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    void Move () {
 
+        //move the player horizontally
+        moveDirection = Input.GetAxis (WalkBtn);
+        animate.SetFloat ("run", Mathf.Abs (moveDirection));
+        physics.velocity = new Vector2 (moveDirection * moveSpeed, physics.velocity.y);
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+
+    void Grounded () {
+        //Detect if the player is on the ground
+        isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+        animate.SetBool ("grounded", isGrounded);
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+
+    //These method transforms a value from a scale to other scale proportionally. Ex: 5 in scaleA (1,10) => becomes =>  50 in scaleB (1,100)  
+    float ChangeNumberScale (float valueToMap, float initialScaleMin, float initialScaleMax, float finalScaleMin, float finalScaleMax) {
+        float MappedValue = finalScaleMin + (valueToMap - initialScaleMin) * (finalScaleMax - finalScaleMin) / (initialScaleMax - initialScaleMin);
+        return MappedValue;
     }
 } //closes player movement class
